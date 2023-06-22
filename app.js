22483c1ce1ac5981587594decb4d60ac4293f85e
Maho Ando <snoopy3janice@gmail.com>
2023-06-22 14:42:20 -0700

    mod

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/api/v1/tasks", taskRoute);
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URL);
-        app.listen(PORT, console.log("server is running."));
+        app.listen(process.env.PORT || PORT, console.log("server is running."));
     } catch (err) {
         console.log(err);
     }
